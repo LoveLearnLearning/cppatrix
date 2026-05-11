aef382f1e89e3d9c9e302b7dfe7c3063176305eb
Yao0454 <yaoyifeng20070524@outlook.com>
2026-05-11 09:18:40 +0800

    Update

--- include/clustering.hpp
+++ include/clustering.hpp
@@ -4,15 +4,26 @@
 #include "functions.hpp"
 #include "matrix.hpp"
 #include <cstddef>
+#include <utility>
 #include <vector>
 namespace clus {
     template<typename T>
     mat::Matrix<T> kmeans(mat::Matrix<T> &data, size_t k) {
         std::vector<mat::Matrix<T>> centroids(k);
+        std::vector<std::pair<mat::Matrix<T>, mat::Matrix<T>>> crowds(data.rows);
         for (auto centroid : centroids) {
             centroid = data.row_view()[size_t(func::randf(0, data.rows))];
         }
-
+        for (size_t i = 0; i < data.rows; ++i) {
+            double min = 100000.f;
+            for (auto centroid : centroids) {
+                double dist = func::dist(data.row_view()[i], centroid);
+                if (dist < min) {
+                    min = dist;
+                    crowds[i] = {data.row_view()[i], centroid};
+                }
+            }
+        }
     }
 }
 
