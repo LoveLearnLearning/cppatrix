1b224933e6161dc9f0d3609286d093ca5ce9e16e
Yao0454 <yaoyifeng20070524@outlook.com>
2026-06-05 19:13:19 +0800

    Update

--- rustrix/src/main.rs
+++ rustrix/src/main.rs
@@ -8,10 +8,20 @@ impl<T: Default + Clone> Matrix<T> {
     fn new(r: usize, c: usize) -> Self {
         Self { rows: (r), cols: (c), data: (vec![T::default(); r * c]) }
     }
+
+    fn from_vec(r: usize, c: usize, _data: Vec<T>) -> Self {
+        assert_eq!(r * c, _data.len());
+        Self { rows: (r), cols: (c), data: (_data) }
+    }
 }
 
 fn main() {
 
-    let m = Matrix::<f32>::new(1, 3);
+    let m1 = Matrix::<f32>::new(1, 3);
+    let mut m2 = Matrix::from_vec(
+        1,
+        2,
+        vec![1, 2],
+    );
 
 }
