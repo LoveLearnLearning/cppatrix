4e766129512862ca66f3251a39ebd12621734586
Yao0454 <yaoyifeng20070524@outlook.com>
2026-06-08 00:17:14 +0800

    Implement Matrix Display and arithmetic ops
    Derive Clone/Debug/PartialEq; simplify Matrix::new bounds
    Add Sub impl and a Mul skeleton; implement fmt::Display
    Update main to use i32, demo addition, and remove unused imports

--- rustrix/src/main.rs
+++ rustrix/src/main.rs
@@ -1,25 +1,16 @@
 use std::{
-    ops::{Add, AddAssign, Div, Mul, Sub},
-    process::Output,
-    result,
+    fmt,
+    ops::{Add, AddAssign, Div, Mul, MulAssign, Sub, SubAssign},
 };
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct Matrix<T> {
     rows: usize,
     cols: usize,
     data: Vec<T>,
 }
 
-impl<T> Matrix<T>
-where
-    T: Default
-        + Clone
-        + Add<Output = T>
-        + Sub<Output = T>
-        + Mul<Output = T>
-        + Div<Output = T>
-        + AddAssign,
-{
+impl<T: Default + Clone> Matrix<T> {
     fn new(r: usize, c: usize) -> Self {
         Self {
             rows: (r),
@@ -40,7 +31,7 @@ where
 
 impl<T: Clone + Add<Output = T> + AddAssign> Add for Matrix<T> {
     type Output = Matrix<T>;
-    fn add(self, rhs: Self) -> Self {
+    fn add(self, rhs: Self) -> Self::Output {
         assert_eq!(self.rows, rhs.rows);
         assert_eq!(self.cols, rhs.cols);
         let mut result = self;
@@ -51,10 +42,48 @@ impl<T: Clone + Add<Output = T> + AddAssign> Add for Matrix<T> {
     }
 }
 
-fn main() {
-    let m1 = Matrix::<f32>::new(1, 3);
-    let mut m2 = Matrix::from_vec(1, 2, vec![1, 2]);
+impl<T: Clone + Sub<Output = T> + SubAssign> Sub for Matrix<T> {
+    type Output = Matrix<T>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        assert_eq!(self.rows, rhs.rows);
+        assert_eq!(self.cols, rhs.cols);
+        let mut result = self;
+        for i in 0..result.data.len() {
+            result.data[i] -= rhs.data[i].clone();
+        }
+        result
+    }
+}
 
-    println!("{:?}", m1.data);
-    m2.data[1] = 3;
+impl<T: Clone + Mul<Output = T>> Mul for Matrix<T> {
+    type Output = Matrix<T>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        assert_eq!(self.cols, rhs.rows);
+    }
+}
+
+impl<T: fmt::Display> fmt::Display for Matrix<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[\n")?;
+        for i in 0..self.rows {
+            write!(f, "    ")?;
+            for j in 0..self.cols {
+                write!(f, "{}", self.data[i * self.cols + j])?;
+                if j + 1 < self.cols {
+                    write!(f, " ")?;
+                }
+            }
+            write!(f, "\n")?;
+        }
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let m1 = Matrix::<i32>::new(1, 3);
+    println!("{}", m1);
+    let mut m2 = Matrix::from_vec(1, 3, vec![1, 2, 3]);
+    m2 = m2 + m1.clone();
+    println!("{}", m1);
+    println!("{}", m2);
 }
