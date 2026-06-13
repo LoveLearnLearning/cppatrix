7ee262b9d7a626e4d0dd173fb204a7e81481d1a4
Yao0454 <yaoyifeng20070524@outlook.com>
2026-06-13 21:30:55 +0800

    Add Tensor header and simplify regression function
    Update build artifacts (.rustc_info.json, target flycheck and
    binary fingerprints) due to code changes

---
+++ include/tensor.hpp
@@ -0,0 +1,17 @@
+#ifndef TENSOR_HPP_
+#define TENSOR_HPP_
+
+#include <cstddef>
+#include <iostream>
+namespace tensor {
+
+template <typename T> class Tensor {
+  private:
+    T *items;
+
+  public:
+};
+
+} // namespace tensor
+
+#endif //  TENSOR_HPP_
--- rustrix/src/regression.rs
+++ rustrix/src/regression.rs
@@ -13,11 +13,4 @@ pub fn forward<T: Default + Clone + Add<Output = T> + AddAssign + Mul<Output = T
     weight.clone() * params.clone() + bais.clone()
 }
 
-pub fn mean_square_equation<T: Default + Clone + Debug>(train: &Matrix<T>, w: T) {
-    let mut diff: f32 = 0.0;
-    let mut cost: f32 = 0.0;
-
-    for row in train.row_iter() {
-        println!("{:?}", row);
-    }
-}
+pub fn mean_square_equation<T: Default + Clone + Debug>(train: &Matrix<T>, w: T) {}
